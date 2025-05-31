9e4f544bafcad68ae74b7649bca9c515b565c234
Shmygol_Maksim <sh.maksim343@gmail.com>
2025-05-31 17:07:27 +0500

    1

--- usage.groovy
+++ usage.groovy
@@ -4,10 +4,10 @@
         stage('Display System Information') {
             steps {
                 script {
-                    def diskInfo = sh(script: 'df -h', returnStdout: true).trim()
+                    def diskInfo = powershell(script: 'Get-Disk | Format-Table -AutoSize | Out-String', returnStdout: true).trim()
                     echo "Disk Usage:\n${diskInfo}"
 
-                    def topProcess = sh(script: 'ps -eo pid,comm,%mem --sort=-%mem | head -n 2 | tail -n 1', returnStdout: true).trim()
+                    def topProcess = powershell(script: 'Get-Process | Sort-Object -Property WS -Descending | Select-Object -First 1 | Format-Table Name,Id,@{Label="Memory(MB)";Expression={[math]::Round($_.WS/1MB,2)}} -AutoSize | Out-String', returnStdout: true).trim()
                     echo "Process with highest memory usage:\n${topProcess}"
                 }
             }
