422fcdc1563129fc9698cd605957aff186afb47b
Shmygol_Maksim <sh.maksim343@gmail.com>
2025-05-31 16:49:19 +0500

    add: script

---
+++ usage.groovy
@@ -0,0 +1,16 @@
+﻿pipeline {
+    agent any
+    stages {
+        stage('Display System Information') {
+            steps {
+                script {
+                    def diskInfo = sh(script: 'df -h', returnStdout: true).trim()
+                    echo "Disk Usage:\n${diskInfo}"
+
+                    def topProcess = sh(script: 'ps -eo pid,comm,%mem --sort=-%mem | head -n 2 | tail -n 1', returnStdout: true).trim()
+                    echo "Process with highest memory usage:\n${topProcess}"
+                }
+            }
+        }
+    }
+}
